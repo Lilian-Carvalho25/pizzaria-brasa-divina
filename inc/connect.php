c3e810169499aba1e0a2e10c0d1eb39786b88e57
Lilian Carvalho <kleidk74@gmail.com>
2023-08-02 19:52:28 -0300

    targeting database

--- inc/connect.php
+++ inc/connect.php
@@ -1,9 +1,14 @@
 <?php 
 
+// $servidor = "localhost";
+// $usuario = "root";
+// $senha = "";
+// $banco = "brasa-divina";
+
 $servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "brasa-divina";
+$usuario = "if0_34739325";
+$senha = "lila2503";
+$banco = "if0_34739325_brasadivina";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 mysqli_set_charset($conexao, "utf8");
